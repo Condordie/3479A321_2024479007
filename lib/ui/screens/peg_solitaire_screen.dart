0fd587ab58dc5780999ed9a583cc9cb826613612
Diego <diego.aven64@gmail.com>
2026-09-04 12:55:49 -0400

    refactoring_y_migración_del_contenido_de_pegcell

--- lib/ui/screens/peg_solitaire_screen.dart
+++ lib/ui/screens/peg_solitaire_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:untitled/core/enums/cell_type.dart';
 import 'package:untitled/ui/screens/RulesScreen.dart';
+import 'package:untitled/ui/widgets/peg_cell.dart';
 
 class PegSolitaireScreen extends StatelessWidget {
   const PegSolitaireScreen({Key? key}) : super(key: key);
@@ -87,57 +88,6 @@ class PegSolitaireScreen extends StatelessWidget {
             },
           ),
         ),
-      ),
-    );
-  }
-}
-
-class PegCell extends StatelessWidget {
-  final int row;
-  final int col;
-  final CellType type;
-  final bool isSelected;
-  final VoidCallback? onTap;
-
-  const PegCell({
-    super.key,
-    required this.row,
-    required this.col,
-    required this.type,
-    this.isSelected = false,
-    this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Colors.grey[400],
-        border: Border.all(color: Colors.grey[600]!, width: 1.5),
-      ),
-      child: Center(
-        child: type == CellType.occupiedPeg
-            ? Container(
-                width: 30,
-                height: 30,
-                decoration: const BoxDecoration(
-                  color: Colors.blue,
-                  shape: BoxShape.circle,
-                ),
-                child: isSelected
-                    ? const Icon(Icons.check, color: Colors.white, size: 20)
-                    : Image.asset('assets/icons/icono.jpg'),
-              )
-            : type == CellType.emptyHole
-                ? Container(
-                    width: 30,
-                    height: 30,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                    ),
-                  )
-                : null,
       ),
     );
   }
